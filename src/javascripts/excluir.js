f890aba627e6768f39bdbfccf5b228082da3cae8
Léo Ferraro <leogianfagna@gmail.com>
2023-10-28 10:42:10 -0300

    comecando a fazer novo form

--- src/javascripts/excluir.js
+++ src/javascripts/excluir.js
@@ -1,24 +1,33 @@
+// conferir id preenchido
 function id(){
     let resultado = false;
     const id = document.getElementById("id").value;
     const ID = parseInt(id);
-    if (ID > 0){
-    resultado = true;
+    
+    if (ID > 0) {
+        resultado = true;
     }
+    
     return resultado; 
 }
+
+// mostrar mensagem de erro
 function showStatusMessage(msg, error){
     var pStatus = document.getElementById("status");
+    
     if (error === true){
-    pStatus.className = "statusError";
-    }else{
-    pStatus.className = "statusSuccess";
+        pStatus.className = "statusError";
+    } else {
+        pStatus.className = "statusSuccess";
     }
+    
     pStatus.textContent = msg;
 }
+
+// fazer exclusão
 function excluir(){
-    if(!id()){
-    showStatusMessage("Preencha o ID...",true);
-    return;
+    if(!id()) {
+        showStatusMessage("Preencha o ID...",true);
+        return;
     }
 }
