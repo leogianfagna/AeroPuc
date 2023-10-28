e1dbef5222a6886304d93830723db5166b896a47
Léo Ferraro <leogianfagna@gmail.com>
2023-10-27 22:00:30 -0300

    adição de javascript para select nas tables

---
+++ src/javascripts/visualizar/select_trajetos.js
@@ -0,0 +1,26 @@
+// Assume que a resposta tem a seguinte estrutura:
+// { status: 'SUCCESS', message: 'Dados obtidos', payload: [ /*...dados...*/ ] }
+
+fetch('http://localhost:3000/listarTrajetos')
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'SUCCESS') {
+            const tabelaDeAeronaves = document.getElementById('tabelaDeAeronaves');
+            tabelaDeAeronaves.innerHTML = ''; // Limpa o conteúdo atual
+
+            data.payload.forEach(rowData => {
+                const tr = document.createElement('tr');
+
+                rowData.forEach(cellData => {
+                    const td = document.createElement('td');
+                    td.textContent = cellData;
+                    tr.appendChild(td);
+                });
+
+                tabelaDeAeronaves.appendChild(tr);
+            });
+        } else {
+            console.error(`Erro ao obter dados: ${data.message}`);
+        }
+    })
+    .catch(error => console.error('Erro ao conectar:', error));
