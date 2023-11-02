cabb4d34db44360f2b79a4953c50a5c9b28171e8
Léo <142264317+leogianfagna@users.noreply.github.com>
2023-11-02 12:20:29 -0300

    documentado e formatado excluir_mapa.js

--- src/javascripts/excluir/excluir_mapa.js
+++ src/javascripts/excluir/excluir_mapa.js
@@ -1,14 +1,16 @@
+// verificar se o id foi preenchido
 function ids(){
     let resultado = false;
     const id = document.getElementById("id").value;
-    const ID = parseInt(id);
     
-    if (ID > 0){
+    if (parseInt(id) > 0) {
         resultado = true;
     }
 
     return resultado; 
 }
+
+// enviar mensagem de erro
 function showStatusMessage(msg, error){
     var pStatus = document.getElementById("status");
     
@@ -17,39 +19,47 @@ function showStatusMessage(msg, error){
     } else {
         pStatus.className = "text-success";
     }
-
+    
     pStatus.textContent = msg;
 }
-function excluir(){
-    if(!ids()){
-    showStatusMessage("ID deve ser preenchido.",true);
-    return;
-    }
-}
-function fetchInserir(body) {
+
+// funcao fetch do tipo deletar
+function fetchDeletar(body) {
     const requestOptions = {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body)
     };
 
-    return fetch('http://localhost:3000/excluirMapa', requestOptions)
+    return fetch('http://localhost:3000/ExcluirAeronave', requestOptions)
     .then(T => T.json())
-  }
-  const idInserido = document.getElementById("id").value;
-  fetchInserir({
-    id: idInserido})
-    .then(resultado => {
-        // obteve resposta, vamos simplesmente exibir como mensagem: 
-        if(resultado.status === "SUCCESS"){
-        showStatusMessage("Mapa de assnetos excluído!", false);
-        }else{
-        showStatusMessage("Erro ao excluir mapa de assentos...: " + message, true);
-        console.log(resultado.message);
-        }
-    })
-    .catch(()=>{
-        showStatusMessage("Erro técnico ao excluir... Contate o suporte.", true);
-        console.log("Falha grave ao excluir.")
-    });
+}
 
+// funcao para excluir
+function excluir(){
+    if (!ids()) {
+        showStatusMessage("ID deve ser preenchido.", true);
+        return;
+    }
+    
+    const idInserido = document.getElementById("id").value;
+    fetchDeletar({
+        // lado esquerdo: as variaveis utilizadas devem ser as mesmas nos arquivos typescript
+        id: idInserido
+     })
+        
+        .then(resultado => {
+            // obteve resposta
+            if (resultado.status === "SUCCESS") {
+                showStatusMessage("Mapa de assentos excluído!", false);
+            } else {
+                showStatusMessage("Erro ao excluir assentos: " + message, true);
+                console.log(resultado.message);
+            }
+        })
+        
+        .catch(()=>{
+            showStatusMessage("Erro técnico ao excluir. Contate o suporte.", true);
+            console.log("Falha grave ao excluir.")
+        });
+}
