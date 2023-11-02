f2882b4661d058e0b9b7d003aaf8d6c2670e2cac
Léo <142264317+leogianfagna@users.noreply.github.com>
2023-11-02 12:16:00 -0300

    documentado e formatado excluir_aeroportos.js

--- src/javascripts/excluir/excluir_aeroportos.js
+++ src/javascripts/excluir/excluir_aeroportos.js
@@ -1,14 +1,16 @@
+// funcao para verificar se o id foi preenchido
 function ids(){
     let resultado = false;
     const id = document.getElementById("id").value;
-    const ID = parseInt(id);
     
-    if (ID > 0){
+    if (parseInt(id) > 0) {
       resultado = true;
     }
 
     return resultado; 
 }
+
+// funcao para exibir mensagem de erro
 function showStatusMessage(msg, error){
     var pStatus = document.getElementById("status");
     
@@ -20,7 +22,9 @@ function showStatusMessage(msg, error){
 
     pStatus.textContent = msg;
 }
-function fetchInserir(body) {
+
+// funcao fetch do tipo delete
+function fetchDeletar(body) {
     const requestOptions = {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json' },
@@ -29,26 +33,33 @@ function fetchInserir(body) {
 
     return fetch('http://localhost:3000/excluirAeroporto', requestOptions)
     .then(T => T.json())
-  }
+}
+
+// funcao de excluir
 function excluir(){
-    if(!ids()){
-    showStatusMessage("ID deve ser preenchido.",true);
-    return;
+    
+    if (!ids()) {
+        showStatusMessage("ID deve ser preenchido.",true);
+        return;
     }
-  const idInserido = document.getElementById("id").value;
-  fetchInserir({
-    id: idInserido})
+    
+    const idInserido = document.getElementById("id").value;
+    fetchDeletar({
+        id: idInserido})
+        
     .then(resultado => {
+        
         // obteve resposta, vamos simplesmente exibir como mensagem: 
-        if(resultado.status === "SUCCESS"){
-        showStatusMessage("Aeroporto excluido!", false);
-        }else{
-        showStatusMessage("Erro ao excluir aeroporto...: " + message, true);
-        console.log(resultado.message);
+        if (resultado.status === "SUCCESS") {
+            showStatusMessage("Aeroporto excluido!", false);
+        } else {
+            showStatusMessage("Erro ao excluir aeroporto: " + message, true);
+            console.log(resultado.message);
         }
     })
+
     .catch(()=>{
-        showStatusMessage("Erro técnico ao excluir... Contate o suporte.", true);
+        showStatusMessage("Erro técnico ao excluir. Contate o suporte.", true);
         console.log("Falha grave ao excluir.")
     });
 }
