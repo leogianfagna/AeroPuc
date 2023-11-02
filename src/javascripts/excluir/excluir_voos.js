cc73179fdb631211a52d13d1cd2751a3f1edc924
Léo <142264317+leogianfagna@users.noreply.github.com>
2023-11-02 12:23:22 -0300

    documentado e formatado excluir_voos.js

--- src/javascripts/excluir/excluir_voos.js
+++ src/javascripts/excluir/excluir_voos.js
@@ -1,14 +1,16 @@
+// verificar se o id foi preenchido
 function ids(){
     let resultado = false;
     const id = document.getElementById("id").value;
-    const ID = parseInt(id);
     
-    if (ID > 0) {
+    if (parseInt(id) > 0) {
         resultado = true;
     }
 
     return resultado; 
 }
+
+// enviar mensagem de erro
 function showStatusMessage(msg, error){
     var pStatus = document.getElementById("status");
     
@@ -17,16 +19,12 @@ function showStatusMessage(msg, error){
     } else {
         pStatus.className = "text-success";
     }
-
+    
     pStatus.textContent = msg;
 }
-function excluir(){
-    if(!ids()){
-        showStatusMessage("ID deve ser preenchido.",true);
-        return;
-    }
-}
-function fetchInserir(body) {
+
+// funcao fetch do tipo deletar
+function fetchDeletar(body) {
     const requestOptions = {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json' },
@@ -37,28 +35,31 @@ function fetchInserir(body) {
     .then(T => T.json())
 }
 
+// funcao para excluir
 function excluir(){
-    if(!ids()){
-        showStatusMessage("ID deve ser preenchido.",true);
+    if (!ids()) {
+        showStatusMessage("ID deve ser preenchido.", true);
         return;
     }
-
+    
     const idInserido = document.getElementById("id").value;
-    fetchInserir({
+    fetchDeletar({
         // lado esquerdo: as variaveis utilizadas devem ser as mesmas nos arquivos typescript
         id: idInserido
      })
+        
         .then(resultado => {
             // obteve resposta
-            if(resultado.status === "SUCCESS"){
-            showStatusMessage("Voo excluído!", false);
-            }else{
-            showStatusMessage("Erro ao excluir voo...: " + message, true);
-            console.log(resultado.message);
+            if (resultado.status === "SUCCESS") {
+                showStatusMessage("Voo excluído!", false);
+            } else {
+                showStatusMessage("Erro ao excluir voo: " + message, true);
+                console.log(resultado.message);
             }
         })
+        
         .catch(()=>{
-            showStatusMessage("Erro técnico ao excluir... Contate o suporte.", true);
+            showStatusMessage("Erro técnico ao excluir. Contate o suporte.", true);
             console.log("Falha grave ao excluir.")
         });
 }
