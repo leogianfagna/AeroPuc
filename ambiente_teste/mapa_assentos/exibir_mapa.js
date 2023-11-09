14964d876bdaa3f5304463a9c45b9ca0d872faf0
Léo <leogianfagna@gmail.com>
2023-11-08 23:18:42 -0300

    iniciando forma de pegar todos os assentos reservados

--- ambiente_teste/mapa_assentos/exibir_mapa.js
+++ ambiente_teste/mapa_assentos/exibir_mapa.js
@@ -1,3 +1,17 @@
-for (let i = 0; i < 10; i++) {
-    document.getElementById("mapaAssentos").innerHTML += "<p>⬛⬜</p>";
-}
+// Faz uma requisição para obter os assentos reservados
+
+fetch('http://localhost:3000/listarAssentosReservados')
+  .then(response => response.json())
+  .then(data => {
+    if (data.status === 'SUCCESS') {
+      const assentosReservados = data.payload.map(row => row.ASSSENTO);
+      console.log('Assentos Reservados:', assentosReservados);
+
+      // Agora você pode manipular o array 'assentosReservados' conforme necessário.
+    } else {
+      console.error('Erro ao obter os assentos reservados:', data.message);
+    }
+  })
+  .catch(error => {
+    console.error('Erro ao fazer a requisição:', error);
+  });
